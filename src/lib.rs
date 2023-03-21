f7b527a81eedc3838f6c6e21dd5d6c778f4415a0
M-Farag <mina.farag@icloud.com>
2023-03-22 01:15:35 +0200

    UPD A rectangle struct for testing

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,25 @@ pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
+struct Rectangle {
+    width:usize,
+    height:usize
+}
+
+impl Rectangle {
+    fn new(width:usize, height:usize) -> Self
+    {
+        Self { width, height }
+    }
+    fn can_hold(&self, other:Rectangle) -> bool {
+        if self.width > other.width && self.height > other.height {
+            true
+        } else {
+            false
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
